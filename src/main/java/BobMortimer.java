02db505a5a21f8e2b2fd0cba61e9ac7174e41c0f
zaki-zulkhairi <zaki.zulkhairi@gmail.com>
2025-08-21 22:05:52 +0800

    Implemented Add,List Feature

--- src/main/java/BobMortimer.java
+++ src/main/java/BobMortimer.java
@@ -8,19 +8,36 @@ public class BobMortimer {
                 "| |_) |  | |_| |  | |_) |      | |  | |  | |_| |  |  _ <     | |      | |    | |  | |  | |___   |  _ <   \n" +
                 "|____/    \\___/   |____/       |_|  |_|   \\___/   |_| \\_\\    |_|      |_|    |_|  |_|  |_____|  |_| \\_\\ :) \n";
         String LINE = "____________________________________________________________";
+        Scanner userInput = new Scanner(System.in);
+        String tasks[] = new String[100];
+        int taskNo = 0;
+
+
+        //Greeting
         System.out.println(LINE + "\n" +
                 "Hello I'm\n" + logo
                 + "\nWhat can I do for you?\n"
                 + LINE + "\n");
 
-        Scanner userInput = new Scanner(System.in);
+        //User input
         while (true) {
             String instruction = userInput.nextLine();
             if(instruction.equals("bye")) {
                 break;
             }
+            else if(instruction.equals("list")) {
+                System.out.println("\n" + LINE + "\n");
+                for(int i=0; i < taskNo; i++) {
+                    System.out.println((i+1) + ". " + tasks[i]);
+                }
+                continue;
+            }
             System.out.println(instruction + "\n" + LINE + "\n");
+            tasks[taskNo] = instruction;
+            taskNo++;
         }
+
+        //exit
         System.out.println("Bye. Hope to see you again soon!\n"
                         + LINE);
     }
